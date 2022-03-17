9af79d4b894d600da943da1572848f8e11bb4973
RafaBrito7 <rafabrito.office@gmail.com>
2022-03-16 21:26:22 -0300

    Criado classes Service do Product e da Category

---
+++ src/main/java/market/services/CategoryService.java
@@ -0,0 +1,17 @@
+package market.services;
+
+import javax.persistence.EntityManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CategoryService {
+
+	private final Logger LOG = LogManager.getLogger(CategoryService.class);
+	
+	private EntityManager entityManager;
+	
+	public CategoryService(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+}
